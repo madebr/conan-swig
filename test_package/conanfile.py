85845cd03c35192c79176d4dc2415feef140cb63
ss1978 <ss1978_hu@yahoo.com>
2019-01-09 11:23:36 +0100

    swig generates files to currentdir

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -9,5 +9,5 @@ import os
 class TestPackageConan(ConanFile):    
     def test(self):
         testdir = os.path.dirname(os.path.realpath(__file__))
-        self.run("swig -python %s"%os.path.join(testdir, "test.i"))
+        self.run("swig -python -outcurrentdir %s"%os.path.join(testdir, "test.i"))
         assert "example.py" in os.listdir(testdir)
