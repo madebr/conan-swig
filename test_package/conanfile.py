ea02a1bcc5607ddd16c20ba0f77ec01af4108182
ss1978 <ss1978_hu@yahoo.com>
2019-01-09 11:44:03 +0100

    test searches generated files in curdir

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -10,4 +10,4 @@ class TestPackageConan(ConanFile):
     def test(self):
         testdir = os.path.dirname(os.path.realpath(__file__))
         self.run("swig -python -outcurrentdir %s"%os.path.join(testdir, "test.i"))
-        assert "example.py" in os.listdir(testdir)
+        assert "example.py" in os.listdir(".")
