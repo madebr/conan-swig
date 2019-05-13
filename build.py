47c9f8090ef22266822b1a05026c809002f3ead9
Anonymous Maarten <anonymous.maarten@gmail.com>
2019-05-13 23:18:15 +0200

    Build swig on windows

--- build.py
+++ build.py
@@ -9,5 +9,13 @@ if __name__ == "__main__":
     arch = os.environ["ARCH"]
 
     builder = build_template_installer.get_builder()
-    builder.add(settings={"os": build_shared.get_os(), "arch_build": arch, })
+
+    # Copy default settings.compiler.* and build_requires
+    builder.add_common_builds()
+    builds = builder.items
+    compiler_props = {k: v for (k, v, ) in builds[0].settings.items() if k.startswith("compiler")}
+    build_requires = builds[0].build_requires
+    builder.items = []
+
+    builder.add(settings={"os_build": build_shared.get_os(), "arch_build": arch, "os": build_shared.get_os(), "arch": arch, **compiler_props}, build_requires=build_requires)
     builder.run()
--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from conans import ConanFile, tools, AutoToolsBuildEnvironment
-import os 
-import shutil
+from conans.errors import ConanInvalidConfiguration
+import os
 
 
 class SwigConan(ConanFile):
@@ -10,39 +10,37 @@ class SwigConan(ConanFile):
     version = "4.0.0"
     description = "SWIG is a software development tool that connects programs written in C and C++ with a variety of high-level programming languages."
 
-    topics = ("conan", "swig", "python", "java", "wrapper")
+    topics = ("conan", "swig", "python", "java", "wrapper", )
     url = "https://github.com/ss1978/conan-swig.git"
     homepage = "http://www.swig.org"
     author = "bincrafters <bincrafters@gmail.com>"
     license = "GPL-3.0"
     exports = ["LICENSE.md"]
-    settings = "os_build", "arch_build", "compiler"
+    settings = "os_build", "arch_build", "compiler", "os", "arch"
+
     _source_subfolder = "source_subfolder"
 
-    @property
-    def _url_filename_sha256_foldername(self):
-        if self.settings.os_build == "Windows":
-            return (
-                "http://prdownloads.sourceforge.net/swig/swigwin-{}.zip".format(self.version),
-                "{}win-{}.zip".format("swig", self.version),
-                "1391aecad92e365b960eb1a1db3866ca1beee61b3395c182298edbf323d1695a",
-                "{}win-{}".format("swig", self.version),
-            )
-        else:
-            return (
-                "https://github.com/swig/swig/archive/rel-{}.tar.gz".format(self.version),
-                "{}-rel-{}.tar.gz".format("swig", self.version),
-                "ab5cbf226ec50855aeca08193fbaafe92fe99b2454848b82f444ec96aa246b47",
-                "{}-rel-{}".format("swig", self.version),
-            )
+    def configure(self):
+        # Verify build configuration
+        if str(self.settings.os_build) != str(self.settings.os):
+            raise ConanInvalidConfiguration("settings.os_build must be equal to settings.os")
+        if str(self.settings.arch_build) != str(self.settings.arch_build):
+            raise ConanInvalidConfiguration("settings.arch_build must be equal to settings.arch_build")
+
+    def package_id(self):
+        del self.info.settings.compiler
+        del self.info.settings.os
+        del self.info.settings.arch
+        self.info.include_build_settings()
 
     def build_requirements(self):
-        if self.settings.os_build != "Windows":
-            self.build_requires("pcre/8.41@bincrafters/stable")
-            if tools.os_info.is_windows:
-                pass
-            else:
-                self.build_requires("bison_installer/3.3.2@bincrafters/stable")
+        if tools.os_info.is_windows:
+            self.build_requires("msys2_installer/20161025@bincrafters/stable")
+        if self.settings.os_build == "Windows":
+            self.build_requires("winflexbison/2.5.18@bincrafters/stable")
+        else:
+            self.build_requires("bison_installer/3.3.2@bincrafters/stable")
+        self.build_requires("pcre/8.41@bincrafters/stable")
 
     def system_requirements(self):
         if self.develop:
@@ -55,42 +53,37 @@ class SwigConan(ConanFile):
                 for package in packages:
                     installer.install(package)
 
-    def build(self):
-        url, filename, sha256, foldername = self._url_filename_sha256_foldername
+    def source(self):
+        url = "https://github.com/swig/swig/archive/rel-{}.tar.gz".format(self.version)
+        sha256 = "ab5cbf226ec50855aeca08193fbaafe92fe99b2454848b82f444ec96aa246b47"
+        foldername = "swig-rel-{}".format(self.version)
+
         tools.get(url, sha256=sha256)
         os.rename(foldername, self._source_subfolder)
 
-        if self.settings.os_build == "Windows":
-            pass
-        else:
-            env_build = AutoToolsBuildEnvironment(self)
-            with tools.chdir(os.path.join(self.build_folder, self._source_subfolder)):
-                self.run('./autogen.sh')
-            args = [
-                "PCRE_LIBS={}".format(" ".join("-l{}".format(lib) for lib in self.deps_cpp_info["pcre"].libs)),
-                "PCRE_CPPFLAGS={}".format(""),
-                "--host={}".format(tools.detected_architecture()),
-            ]
-            env_build.configure(configure_dir=os.path.join(self.build_folder, self._source_subfolder), args=args)
-            env_build.make()
+    def build(self):
+        with tools.chdir(os.path.join(self.build_folder, self._source_subfolder)):
+            self.run('./autogen.sh', win_bash=tools.os_info.is_windows)
+        env_build = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+        if tools.os_info.is_windows:
+            env_build.link_flags.append("-static")
+        args = [
+            "PCRE_LIBS={}".format(" ".join("-l{}".format(lib) for lib in self.deps_cpp_info["pcre"].libs)),
+            "PCRE_CPPFLAGS={}".format(" ".join("-D{}".format(define) for define in self.deps_cpp_info["pcre"].defines)),
+            "--host={}".format(tools.detected_architecture()),
+            "--enable-cpp11-testing",
+        ]
+        env_build.configure(configure_dir=os.path.join(self.build_folder, self._source_subfolder), args=args)
+        env_build.make()
 
     def package(self):
-        self.copy(pattern="LICENSE", src=self._source_subfolder, dst="licenses")
-        if self.settings.os_build == "Windows":
-            self.copy("swig.exe", src=os.path.join(self.build_folder, self._source_subfolder), dst="bin")
-            shutil.copytree(os.path.join(self.build_folder, self._source_subfolder, "Lib"), os.path.join(self.package_folder, "share", "swig", self.version))
-        else:
-            with tools.chdir(self.build_folder):
-                env_build = AutoToolsBuildEnvironment(self)
-                env_build.install()
-                with tools.chdir(os.path.join(self.package_folder, "bin")):
-                    self.run("strip swig")
-                    self.run("strip ccache-swig")
-
-    def package_id(self):
-        del self.info.settings.compiler
-        if self.settings.os_build == "Windows":
-            del self.info.settings.arch_build
+        with tools.chdir(self.build_folder):
+            env_build = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+            env_build.install()
+            with tools.chdir(os.path.join(self.package_folder, "bin")):
+                ext = ".exe" if tools.os_info.is_windows else ""
+                self.run("strip swig{}".format(ext), win_bash=tools.os_info.is_windows)
+                self.run("strip ccache-swig{}".format(ext), win_bash=tools.os_info.is_windows)
 
     def package_info(self):
         bindir = os.path.join(self.package_folder, "bin")
