6062b98be2e584af66fa81cdca0df3c385faeac4
Anonymous Maarten <anonymous.maarten@gmail.com>
2019-05-15 19:27:17 +0200

    Even more complex build.py

--- build.py
+++ build.py
@@ -9,5 +9,11 @@ if __name__ == "__main__":
     arch = os.environ["ARCH"]
 
     builder = build_template_installer.get_builder()
-    builder.add(settings={"os_build": build_shared.get_os(), "arch_build": arch, "os": build_shared.get_os(), "arch": arch})
+
+    builder.add_common_builds()
+    builds = builder.items
+    compiler_props = {k: v for (k, v, ) in builds[0].settings.items() if k.startswith("compiler")}
+    builder.items = []
+
+    builder.add(settings={"os_build": build_shared.get_os(), "arch_build": arch, "os": build_shared.get_os(), "arch": arch, **compiler_props})
     builder.run()
