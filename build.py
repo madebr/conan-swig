d5672a0a30b25d8a533db3a03063c94aeca30ec0
Croydon <git@cr0ydon.com>
2019-09-25 23:49:42 +0200

    Use VS on Windows instead of MinGW

--- build.py
+++ build.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 from bincrafters import build_template_installer, build_shared
 import os
 
-
 if __name__ == "__main__":
+
     arch = os.environ["ARCH"]
-
     builder = build_template_installer.get_builder()
-
-    # Copy default settings.compiler.* and build_requires
-    builder.add_common_builds()
-    builds = builder.items
-    compiler_props = {k: v for (k, v, ) in builds[0].settings.items() if k.startswith("compiler")}
-    build_requires = builds[0].build_requires
-    builder.items = []
-
-    builder.add(settings={"os_build": build_shared.get_os(), "arch_build": arch, "os": build_shared.get_os(), "arch": arch, **compiler_props}, build_requires=build_requires)
+    builder.add({"os" : build_shared.get_os(), "arch_build" : arch, "arch": arch}, {}, {}, {})
     builder.run()
